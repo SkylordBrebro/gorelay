90151d30520ce99c52b54ee6ed8641279c592915
brenc <bren.cooper.brown@gmail.com>
2025-02-24 05:57:23 -0500

    Fix structure. add hello world.

--- plugins/example/example.go
+++ plugins/example/example.go
@@ -16,17 +16,18 @@ var Plugin ExamplePlugin
 // Initialize sets up the plugin
 func (p *ExamplePlugin) Initialize(c *client.Client) error {
 	p.client = c
+	p.client.GetLogger().Info("HelloWorld", "Plugin initialized!")
 	return nil
 }
 
 // Name returns the plugin name
 func (p *ExamplePlugin) Name() string {
-	return "Example"
+	return "HelloWorld"
 }
 
 // Author returns the plugin author
 func (p *ExamplePlugin) Author() string {
-	return "GoRelay Team"
+	return "Example Author"
 }
 
 // Version returns the plugin version
@@ -36,30 +37,32 @@ func (p *ExamplePlugin) Version() string {
 
 // Description returns the plugin description
 func (p *ExamplePlugin) Description() string {
-	return "An example plugin demonstrating the plugin system"
+	return "A Hello World plugin that demonstrates logging"
 }
 
 // OnEnable is called when the plugin is enabled
 func (p *ExamplePlugin) OnEnable() error {
+	p.client.GetLogger().Info("HelloWorld", "Hello, World! Plugin enabled and ready to go!")
 	return nil
 }
 
 // OnDisable is called when the plugin is disabled
 func (p *ExamplePlugin) OnDisable() error {
+	p.client.GetLogger().Info("HelloWorld", "Goodbye, World! Plugin shutting down...")
 	return nil
 }
 
 // OnMapInfo is called when a MapInfo packet is received
 func (p *ExamplePlugin) OnMapInfo(packet *packets.MapInfoPacket) {
-	p.client.GetLogger().Info("Example", "Entered map: %s", packet.Name)
+	p.client.GetLogger().Info("HelloWorld", "Hello from map: %s!", packet.Name)
 }
 
 // OnNewTick is called when a NewTick packet is received
 func (p *ExamplePlugin) OnNewTick(packet *packets.NewTickPacket) {
-	// Handle game tick updates
+	// We'll leave this empty to avoid spam
 }
 
 // OnUpdate is called when an Update packet is received
 func (p *ExamplePlugin) OnUpdate(packet *packets.UpdatePacket) {
-	// Handle entity updates
+	// We'll leave this empty to avoid spam
 }
